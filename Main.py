9389e6c670d7b701fc83101c096dd0f2ae699e1d
Marvin Petsch <marvinpetsch@mailbox.org>
2023-11-24 19:59:31 +0100

    api request with context loaded

--- Main.py
+++ Main.py
@@ -1,12 +1,30 @@
 from openai import OpenAI
+
 client = OpenAI()
 
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts in a creative but concise way."},
-    {"role": "user", "content": "Compose a short poem that explains the concept of recursion in programming."}
-  ]
-)
+messages = [
+    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts in " +
+                                  "a creative but concise way."}
+]
 
-print(completion.choices[0].message)
+
+def new_chat():
+    for message in messages[1:]:
+        messages.pop(message)
+
+
+def ask(user_input):
+    messages.append({"role": "user", "content": user_input})
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+    response = completion.choices[0].message.content
+    print(response + "\n-----")
+    messages.append({"role": "assistant", "content": response})
+
+
+ask("write a short poem about the singleton design pattern")
+
+
+ask("make it shorter")
