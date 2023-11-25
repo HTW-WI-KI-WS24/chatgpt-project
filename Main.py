ca063e54ef37e6a297b4146f5545d52abf0300b3
PetschMarvin <marvinpetsch@mailbox.org>
2023-11-25 11:07:50 +0100

    cleaned up some code

--- Main.py
+++ Main.py
@@ -1,22 +1,46 @@
 import customtkinter as ctk
+import time
 from openai import OpenAI
 
 client = OpenAI()
 
-messages = [
+chatgpt_context = [
     {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts in " +
                                   "a creative but concise way."}
 ]
 
-chat_message_labels = []
+chat_message_gui_labels = []
+
+def get_chatgpt_response(user_request):
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=chatgpt_context
+    )
+
+    return completion.choices[0].message.content
+
+def generate_chatgpt_response(user_request):
+    user_input_textbox.delete('1.0', "end")
+    display_user_request_in_gui(user_request)
+    time.sleep(1)
+
+    chatgpt_context.append({"role": "user", "content": user_request})
+
+    chatgpt_response = get_chatgpt_response()
+
+    display_chatgpt_response_in_gui(chatgpt_response)
+
+    chatgpt_context.append({"role": "assistant", "content": chatgpt_response})
 
 def new_chat():
-    for msg in messages[1:]:
-        messages.remove(msg)
-    for chat_message_label in chat_message_labels:
+    for item in chatgpt_context[1:]: # remove everything besides the first input (which is the role assignment)
+        chatgpt_context.remove(item)
+
+    # gui
+    for chat_message_label in chat_message_gui_labels:
         chat_message_label.destroy()
 
-def send_user_request(user_request):
+def display_user_request_in_gui(user_request):
     user_request_label = ctk.CTkLabel(
         chat_frame,
         corner_radius=20,
@@ -25,31 +49,22 @@ def send_user_request(user_request):
         text=user_request,
         anchor="center"
     )
-    chat_message_labels.append(user_request_label)
     user_request_label.pack(fill="x", padx=70, pady=(35, 0), ipadx=10, ipady=10)
-    user_input_textbox.delete('1.0', "end")
+    chat_message_gui_labels.append(user_request_label)
 
-    generate_chatgpt_response(user_request)
-
-def generate_chatgpt_response(user_input):
-    messages.append({"role": "user", "content": user_input})
-    completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=messages
-    )
-    response = completion.choices[0].message.content
-    messages.append({"role": "assistant", "content": response})
+def display_chatgpt_response_in_gui(chatgpt_response):
     chatgpt_response_label = ctk.CTkLabel(
         chat_frame,
         corner_radius=20,
         fg_color="#383d38",
         font=('Arial', 16, 'normal'),
-        text=response,
+        text=chatgpt_response,
         anchor="center"
     )
-    chat_message_labels.append(chatgpt_response_label)
     chatgpt_response_label.pack(fill="x", padx=70, pady=(35, 0), ipadx=10, ipady=10)
+    chat_message_gui_labels.append(chatgpt_response_label)
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 window = ctk.CTk()
 window.title("BookGPT")
 window.geometry("800x600")
@@ -78,7 +93,7 @@ send_request_button = ctk.CTkButton(
     input_frame,
     text="Anfrage senden",
     fg_color="#3c383d",
-    command=lambda: send_user_request(user_input_textbox.get("1.0", "end"))
+    command=lambda: generate_chatgpt_response(user_input_textbox.get("1.0", "end"))
 )
 send_request_button.grid(column=0, row=1, padx=70, pady=(20, 50), sticky="e")
 
@@ -91,3 +106,4 @@ new_chat_button = ctk.CTkButton(
 new_chat_button.grid(column=0, row=1, padx=70, pady=(20, 50), sticky="w")
 
 window.mainloop()
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
