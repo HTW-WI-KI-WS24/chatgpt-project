4f438e3e7b65b66ec10a973de91d5e59a546ee90
PetschMarvin <marvinpetsch@mailbox.org>
2023-11-25 11:29:32 +0100

    agent directory und central agent

---
+++ agents/CentralAgent.py
@@ -0,0 +1,7 @@
+class CentralAgent():
+    def __init__(self):
+        self.name = "CentralAgent"
+        self.context = [
+            {"role": "system", "content": "You are a skilled book writer that knows what a good book needs right from the start."}
+        ]
+
